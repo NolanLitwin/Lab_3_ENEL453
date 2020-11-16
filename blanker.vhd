dd206aa77a1d277de63d0e09e5eceee534439b50
ben-krett <56660749+ben-krett@users.noreply.github.com>
2020-11-16 14:18:27 -0700

    finished up the blanker

--- blanker.vhd
+++ blanker.vhd
@@ -16,10 +16,29 @@ architecture Behaviour of Blanker is
 		process (clk)
 			begin
 				if rising_edge(clk) then
-					if ((mode = "00") or (mode = "11")) then
-						blank_out <= "110000"; -- no decimal point
+					blank_out (5 downto 4) <= "11"; -- first two always blanked
+					if ((mode = "00")) then
+						blank_out(3 downto 0) <= "0000"; -- no decimal point
+					elsif (mode = "11") then
+						blank_out (0) <= '0'; -- last two never blanked
+						if(hex_in(15 downto 12) = "0000") then
+							blank_out (3) <= '1';
+							if(hex_in(11 downto 8) = "0000" ) then
+								blank_out (2) <= '1';
+								if(hex_in(7 downto 4) = "0000" ) then
+									blank_out (1) <= '1';
+								else
+									blank_out (1) <= '0';
+								end if;
+							else
+								blank_out (2) <= '0';
+							end if;
+						else
+							blank_out (3) <= '0';
+						end if;
+							
+							
 					else
-						blank_out (5 downto 4) <= "11"; -- first two always blanked
 						if ((hex_in(15 downto 12) = "0000" ) and (dp_in(3) = '0')) then --if leading digit is 0, and there is no decimal point there
 							blank_out (3 downto 0) <= "1000";
 						else
